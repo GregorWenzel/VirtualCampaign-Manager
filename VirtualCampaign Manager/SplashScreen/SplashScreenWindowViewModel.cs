d2277e566ddc02bb73aae754bb1feb7ce6336f54
GregorWenzel <gregor.wenzel@me.com>
2018-03-22 17:17:19 +0100

    Added SplashScreen with ini checking

--- VirtualCampaign Manager/SplashScreen/SplashScreenWindowViewModel.cs
+++ VirtualCampaign Manager/SplashScreen/SplashScreenWindowViewModel.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using ComponentPro.IO;
+using ComponentPro.Net;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
+using VirtualCampaign_Manager.Data;
 using VirtualCampaign_Manager.MainHub;
 
 namespace VirtualCampaign_Manager.SplashScreen
@@ -14,6 +20,8 @@ namespace VirtualCampaign_Manager.SplashScreen
     {
         public EventHandler<EventArgs> SuccessEvent;
         public EventHandler<EventArgs> FailureEvent;
+
+        public string ErrorString { get; set; } = "";
 
         public string VersionString
         {
@@ -68,16 +76,155 @@ namespace VirtualCampaign_Manager.SplashScreen
 
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            stepCounter += 1;
+            if (stepCounter > 0)
+            {
+                if (stepCounter < statusStringArr.Length / 2)
+                {
+                    StatusString = statusStringArr[stepCounter, 0] + "...";
+                    CheckSetting(statusStringArr[stepCounter, 0], statusStringArr[stepCounter, 1]);
+                }
+                else
+                {
+                    timer.Stop();
 
-            if (stepCounter < statusStringArr.Length/2)
-            {
-                StatusString = statusStringArr[stepCounter, 0] + "...";
+                    if (ErrorString.Length == 0)
+                    {
+                        SuccessEvent?.Invoke(this, new EventArgs());
+                    }
+                    else
+                    {
+                        MessageBox.Show(ErrorString, "Errors in settings file detected!", MessageBoxButton.OK);
+                        Application.Current.Dispatcher.Invoke((Action)delegate
+                        {
+                            Process.Start(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Settings.ini"));
+                            Environment.Exit(-1);
+                            //Application.Current.Shutdown();
+                            return;
+                        });
+                    }
+                }
             }
-            else
+
+            stepCounter++;
+
+        }
+
+        private void CheckSetting(string settingKey, string settingValue)
+        {
+            string url = "";
+            LoginData loginData = null;
+
+            switch (settingKey)
             {
-                SuccessEvent?.Invoke(this, new EventArgs());
+                case "Base path": // Settings.LocalBasePath
+                case "Production path": // Settings.LocalProductionPath
+                case "FFmpeg path": // Settings.LocalFfmpegExePath
+                case "Audio path": //Settings.LocalAudioPath
+                case "Fusion plugin path": //Settings.LocalFusionPluginPath
+                case "Deadline executable path": //Settings.LocalDeadlineExePath 
+                    string extension = Path.GetExtension(settingValue);
+                    if (extension.Length == 0)
+                    {
+                        if (!Directory.Exists(settingValue))
+                        {
+                            ErrorString += string.Format("- Directory '{0}' does not exist.\r\n", settingValue);
+                        }
+                    }
+                    else
+                    {
+                        if (!File.Exists(settingValue))
+                        {
+                            ErrorString += string.Format("- File '{0}' does not exist.\r\n", settingValue);
+                        }
+                    }
+                    break;
+                case "Server url"://Settings.ServerUrl
+                case "Services url": //Settings.ServicesUrl
+                case "Film url": // Settings.ServerUrl
+                    url = settingValue;
+                    break;
+                case "Remote user directory":// "Settings.FtpUserDirectoryLogin"
+                    loginData = Settings.FtpUserDirectoryLogin;
+                    break;
+                case "Remote audio directory"://"Settings.FtpAudioDirectoryLogin"
+                    loginData = Settings.FtpAudioDirectoryLogin;
+                    break;
+                case "Remote HASH directory"://"Settings.FtpHashDirectoryLogin"
+                    loginData = Settings.FtpHashDirectoryLogin;
+                    break;
+                case "Remote product preview directory":// Settings.FtpProductPreviewDirectoryLogin
+                    loginData = Settings.FtpProductPreviewDirectoryLogin;
+                    break;
+                case "SALTED string": //Settings.SALTED
+                    if (settingValue.Length == 0)
+                    {
+                        ErrorString += "No SALTED pass defined.\r\n";
+                    }
+                    break;
+                case "Email server settings": //"Settings.EmailServerLogin"
+                    break;
             }
+
+            if (url.Length > 0)
+            {
+                if (!IsUrlReachable(url))
+                {
+                    ErrorString += string.Format("- URL '{0}' is not reachable.\r\n", url);
+                }
+            }
+            else if (loginData != null)
+            {
+                string ftpLoginResult = IsSFtpServerAvailable(loginData);
+                if (ftpLoginResult.Length > 0)
+                {
+                    ErrorString += ftpLoginResult;
+                }
+            }
+        }
+
+        private bool IsUrlReachable(string url)
+        {
+            if (!url.Contains("http://"))
+            {
+                url = "http://" + url;
+            }
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Timeout = 15000;
+            request.Method = "HEAD"; // As per Lasse's comment
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    return response.StatusCode == HttpStatusCode.OK;
+                }
+            }
+            catch (WebException ex)
+            {
+                return false;
+            }
+        }
+
+        private string IsSFtpServerAvailable(LoginData loginData)
+        {
+            Sftp ftpClient = new Sftp();
+            ftpClient.Connect(loginData.Url);
+
+            if (!ftpClient.IsConnected)
+            {
+                return string.Format("- Cannot connect to ftp server '{0}'", loginData.Url);
+            }
+
+            try
+            {
+                ftpClient.Authenticate(loginData.Username, loginData.Password);
+            }
+            catch (Exception ex)
+            { 
+                ftpClient.Disconnect();
+                return string.Format("- Invalid credentials for server '{0}', username '{1}'", loginData.Url, loginData.Username);
+            }
+            ftpClient.Disconnect();
+            return "";
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
